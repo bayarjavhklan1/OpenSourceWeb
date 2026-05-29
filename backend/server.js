2caa7c7a6e24b752b5fbdfabcec644b9e6533f75
Jae min <minjaejeong84@gmail.com>
2026-05-29 23:36:11 +0900

    일단 server와 스키마 구성 먼저 올림

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import authRouter from './routes/auth.js';
+
+var app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/Open_Web_proj')
+  .then(function() {
+    console.log('MongoDB 연결 성공!');
+  })
+  .catch(function(err) {
+    console.error('MongoDB 연결 실패:', err);
+  });
+
+app.use('/auth', authRouter);
+
+app.listen(5000, function() {
+  console.log('서버 실행 중 → http://localhost:5000');
+});
